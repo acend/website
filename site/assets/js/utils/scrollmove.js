326716dd27f7a974751aedb29fc1bbe3093c86d1
jan <mail@fint.ch>
2024-09-16 13:35:29 +0200

    buttons

---
+++ site/assets/js/utils/scrollmove.js
@@ -0,0 +1,18 @@
+//import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+  
+export default function() {
+
+  const progress = document.querySelector('.scrollmove');
+
+  if (!progress) return;
+
+  progress.animate([
+    { transform: 'translateX(0%)' },
+    { transform: 'translateX(-300%)' }
+  ], {
+    timeline: new ScrollTimeline({
+      source: document.documentElement
+    })
+  });
+}
