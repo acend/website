1ddbccf631991634f27eb3798f7e3f88d7eae0e5
jan <mail@fint.ch>
2025-12-05 14:46:44 +0100

    finetuning scroll-content about page

--- site/assets/js/utils/scrollmove.js
+++ site/assets/js/utils/scrollmove.js
@@ -1,21 +1,27 @@
 //import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
 
-  
+
 export default function() {
 
-  const progress = document.querySelector('.scrollmove');
+  if (window.innerWidth < 768) return;
 
-  if (!progress) return;
+  const scrollEl = document.querySelector('.scrollmove');
 
-  progress.animate([
-    { transform: 'translateX(0%)' },
-    { transform: 'translateX(-300%)' }
-  ], {
-    timeline: new ScrollTimeline({
-      source: document.documentElement
-    })
+  if (!scrollEl) return;
+
+    const container  = scrollEl.parentElement
+
+  // get element widths
+  const scrollElWidth = scrollEl.scrollWidth - container.clientWidth;
+
+  scrollEl.animate(
+   { transform: ['translateX(0%)','translateX(-' + scrollElWidth + 'px)'] }, {
+    timeline: new ViewTimeline({ subject: container }),
+    rangeStart: { rangeName: 'contain', offset: CSS.percent(10) },
+    rangeEnd: { rangeName: 'contain', offset: CSS.percent(80) },
+    fill: 'both',
   });
 
 
- 
+
 }
